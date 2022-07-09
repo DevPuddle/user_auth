4e61b2d83a1ce4522c2463073a7c047709cb38d8
Isaiah Henson <Devpuddles@gmail.com>
2022-07-08 21:52:55 -0400

    registerd a user, the route works

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require('./db.js')
 connectDB()
 
 app.use(express.json())
+app.use('/api/Auth', require('./Auth/Routes'))
 
 const server = app.listen(PORT, () => {
     console.log(`The server is running on ${PORT} ya did it`)
